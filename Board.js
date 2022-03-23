b26b0b005bebb4311746ff54d422ba369b13f6f8
melchior321 <mplznr@gmail.com>
2022-03-23 13:45:10 +0100

    Meningful commit

--- Board.js
+++ Board.js
@@ -1,7 +1,8 @@
 class Board {
+    static board
 
-    static draw() {
-        return board = [
+    static initializeBoard() {
+        board = [
             [Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board],
             [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
             [Element.Board, , Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board, Element.Board, , Element.Board],
@@ -9,21 +10,21 @@ class Board {
             [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
             [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
             [Element.Board, , , , , , , , , , Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, , , , , Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
-            [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , , , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , , , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , , , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , , , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, , , , , , Element.Board, Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
+            [Element.Board, , , , , , , , , , Element.Board, Element.Board, Element.Board, Element.Board, , , Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, , , , , , Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, , , Element.Board, , , , , , , , Element.Board],
             [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
             [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
             [Element.Board, , Element.Board, Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board, Element.Board, , Element.Board],
@@ -31,6 +32,36 @@ class Board {
             [Element.Board, , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , , Element.Board],
             [Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board, Element.Board],
           ];
+    }
+
+    static draw() {
+        return board
+    }
+
+    static addFood(numberOfFood) {
+        let numberOfEmptyFields = 0;
+        for (let y = 0; y < 28; y++) {
+            for (let x = 0; x < 50; x++) {
+                if (board[y][x]) {
+                    numberOfEmptyFields++;
+                }
+            }
+        }
+        
+        let numberOfFieldToAssFood = Math.random() * numberOfEmptyFields +1
+        let yOfFoodField = 0
+        let xOfFoodField = 0
+        for (let y = 0; y < 28; y++) {
+            for (let x = 0; x < 50; x++) {
+                if (board[y][x] && numberOfFieldToAssFood > 0) {
+                    yOfFoodField = y
+                    xOfFoodField = x
+                    numberOfFieldToAssFood--;
+                }
+            }
+        }
+
+        board[yOfFoodField][xOfFoodField] = Element.Food
     }
     
 }
